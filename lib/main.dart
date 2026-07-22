895dc1ad35e82d895e157dbe677e49c4fe6ab3fd
MarioSQuilantan <marios0013@outlook.com>
2026-07-22 12:23:24 -0600

    first: commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
+  runApp(const PokedexApp());
+}
+
+class PokedexApp extends StatelessWidget {
+  const PokedexApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final router = getIt<AppRouter>().router;
+
+    return MaterialApp.router(
+      title: 'Pokedex App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+      ),
+      routerConfig: router,
+    );
+  }
+}
